7d71af0508d72299e0a122f15c38b0a77aa803de
Arina Rodina <rodinaarisha@yandex.ru>
2023-04-26 16:51:53 +0400

    added new locators

--- locators.py
+++ locators.py
@@ -10,6 +10,7 @@ class TestLocators:
     issue_for_incorrect_password = By.XPATH, ".//p[@class='input__error text_type_main-default']"   # Ошибка для некорректного пароля
     field_email = By.XPATH, "//label[text()='Email']/following-sibling::input"  # Поле email на экране входа
     button_input = By.XPATH, ".//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']"  # Кнопка Войти
+    button_input_registration = By.LINK_TEXT, 'Войти'  # Кнопка "Войти" на странице регистрации
     button_personal_account = By.LINK_TEXT, 'Личный Кабинет'  # Кнопка Личный кабинет
     button_exit = By.XPATH, ".//button[@class='Account_button__14Yp3 text text_type_main-medium text_color_inactive']"  # Кнопка Выход на странице Личного кабинета
     button_history_shop = (By.XPATH, ".//li[@class='Account_listItem__35dAP']/a[@class='Account_link__2ETsJ text text_type_main-medium text_color_inactive']")  # Кнопка история заказов на странице Личного кабинета
