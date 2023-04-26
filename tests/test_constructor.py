afe5a93868297e4cda6c820056e9a32657489527
Arina Rodina <rodinaarisha@yandex.ru>
2023-04-26 17:22:30 +0400

    Changed test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,46 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from locators import TestLocators
 
 
-def test_constructor_switch_to_sauces_tab():
-    chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--window-size=1000,992')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://stellarburgers.nomoreparties.site")
-    sauces = driver.find_element(By.XPATH, ".//span[@class='text text_type_main-default' and text()='Соусы']")
-    sauces.click()
-    sauces_label=driver.find_element(By.XPATH,".//h2[@class='text text_type_main-medium mb-6 mt-10' and text()='Соусы']")
-    example_sauce=driver.find_element(By.XPATH, ".//p[@class='BurgerIngredient_ingredient__text__yp3dH' and text()='Соус Spicy-X']")
-    assert sauces_label.text =='Соусы' and example_sauce.text=='Соус Spicy-X'
-    driver.quit()
+class TestConstructor:
 
+    def test_constructor_switch_to_sauces_tab(self, driver):
+        driver.find_element(*TestLocators.button_sauces).click()
+        title_sauce = driver.find_element(*TestLocators.title_sauce)
+        example_sauce = driver.find_element(*TestLocators.example_sauce_spicy_x)
+        assert title_sauce.text == 'Соусы' and example_sauce.text == 'Соус Spicy-X', 'Название раздела называется "Соусы" и  Соус Spicy-X отображается'
 
-def test_constructor_switch_to_fillings_tab():
-    chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--window-size=1000,992')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://stellarburgers.nomoreparties.site")
-    fillings = driver.find_element(By.XPATH, ".//span[@class='text text_type_main-default' and text()='Начинки']")
-    fillings.click()
-    fillings_label=driver.find_element(By.XPATH,".//h2[@class='text text_type_main-medium mb-6 mt-10' and text()='Начинки']")
-    example_fillings=driver.find_element(By.XPATH, ".//p[@class='BurgerIngredient_ingredient__text__yp3dH' and text()='Говяжий метеорит (отбивная)']")
-    assert fillings_label.text =='Начинки' and example_fillings.text=='Говяжий метеорит (отбивная)'
-    driver.quit()
+    def test_constructor_switch_to_fillings_tab(self, driver):
+        driver.find_element(*TestLocators.button_fillings).click()
+        title_fillings = driver.find_element(*TestLocators.title_fillings)
+        example_fillings = driver.find_element(*TestLocators.example_fillings_steak)
+        assert title_fillings.text == 'Начинки' and example_fillings.text == 'Говяжий метеорит (отбивная)', 'Название раздела называется "Начинки" и  Говяжий метеорит (отбивная) отображается'
 
-
-def test_constructor_switch_to_fillings_tab():
-    chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--window-size=1000,992')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://stellarburgers.nomoreparties.site")
-    fillings = driver.find_element(By.XPATH, ".//span[@class='text text_type_main-default' and text()='Начинки']")
-    fillings.click()
-    breads = driver.find_element(By.XPATH, ".//span[@class='text text_type_main-default' and text()='Булки']")
-    breads.click()
-    breads_label = driver.find_element(By.XPATH,".//h2[@class='text text_type_main-medium mb-6 mt-10' and text()='Булки']")
-    example_breads = driver.find_element(By.XPATH, ".//p[@class='BurgerIngredient_ingredient__text__yp3dH' and text()='Флюоресцентная булка R2-D3']")
-    assert breads_label.text =='Булки' and example_breads.text =='Флюоресцентная булка R2-D3'
-    driver.quit()
+    def test_constructor_switch_to_breads_tab(self, driver):
+        driver.find_element(*TestLocators.button_fillings).click()
+        driver.find_element(*TestLocators.button_breads).click()
+        title_breads = driver.find_element(*TestLocators.title_breads)
+        example_breads = driver.find_element(*TestLocators.example_breads_r2_d3)
+        assert title_breads.text == 'Булки' and example_breads.text == 'Флюоресцентная булка R2-D3', 'Название раздела называется "Булки" и  Флюоресцентная булка R2-D3 отображается'
